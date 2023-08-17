657a54d2ece7985f35e1595e7c5ace3f4bfd3280
AleGxrcia <fagarciatavarez@gmail.com>
2023-08-16 23:16:18 -0400

    Add user model and route

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+    },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: function(value) {
+        return /^(?=.*\d)[A-Za-z\d]{6,}$/.test(value);
+      },
+      message: 'La contraseña debe contener al menos 6 caracteres y al menos un numero.'
+      }
+    }
+});
+
+const User = model("Users", userSchema);
+
+module.exports = User;
