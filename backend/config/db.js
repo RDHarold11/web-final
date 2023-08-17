75379d0122911a853e97f41e4ba077ee49c8e5b6
Carlos Daniel Taveras Liranzo <20212021@itla.edu.do>
2023-08-17 00:04:00 -0400

    MongoDB Atlas is added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const MongoDBServer = "mongodb://127.0.0.1:27017/local";
+const MongoDBServer = "mongodb+srv://admin:admin@web.wuwa480.mongodb.net/WebProjectretryWrites=true&w=majority";
 async function connectDB() {
   try {
     await mongoose.connect(MongoDBServer, {
